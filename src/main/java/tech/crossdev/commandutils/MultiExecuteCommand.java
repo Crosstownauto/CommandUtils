3e5d17330d4f9a49d720a802786065547740f7ba
CrossTownAuto <101027754+Crosstownauto@users.noreply.github.com>
2024-02-18 23:15:37 +0000

    Implemented the ./multiex command
    Despite this being a terrible implementation it works. Cleanup is later and the theory is right now.

--- src/main/java/tech/crossdev/commandutils/MultiExecuteCommand.java
+++ src/main/java/tech/crossdev/commandutils/MultiExecuteCommand.java
@@ -1,15 +1,45 @@
 package tech.crossdev.commandutils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.logging.Level;
 
 public class MultiExecuteCommand implements CommandExecutor {
 
+    // TODO: Clean this entire function up, it's absolutely horrible.
+    // TODO: Also document this relevantly.
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings)
     {
-        System.out.println("Got command!");
+        if(strings.length == 0) return false;
+
+        if (commandSender instanceof Player) {
+            Player player = (Player) commandSender;
+
+            if (player.isOp())
+
+            Bukkit.getLogger().log(Level.INFO, "Running all commands");
+
+            for (int i = 0; i < strings.length; i++)
+            {
+                String[] cmd = strings[i].split("-");
+
+                // TODO: Run commands at the same elevation of the person running it.
+                String commandToRun = "";
+
+                for (int j = 0; j < cmd.length; j++) {
+                    if(j == 0) commandToRun += cmd[j];
+                    else commandToRun += " " + cmd[j];
+                }
+
+                Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), commandToRun);
+            }
+        }
+
         return true;
     }
 
