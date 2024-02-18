67b3ef9c885636ee317baef44be05701a8c1078f
CrossTownAuto <101027754+Crosstownauto@users.noreply.github.com>
2024-02-18 18:23:00 +0000

    Init commit
    Added basic test along with it to ensure that it was working with my client and server.

---
+++ src/main/java/tech/crossdev/commandutils/CommandUtils.java
@@ -0,0 +1,39 @@
+package tech.crossdev.commandutils;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
+import org.bukkit.Bukkit;
+
+public final class CommandUtils extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        /* Attempt to register the plugin as a listener to bukkit events. */
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+
+        // TODO: Implement shutdown logic (lmao)
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event)
+    {
+        /* Obtain player from event and check their permission. If they're privileged, continue.  */
+
+        Player joinedPlayer = event.getPlayer();
+
+        if(!joinedPlayer.isOp()) return;
+
+        joinedPlayer.sendMessage("Hello!" + joinedPlayer.getName());
+    }
+}
